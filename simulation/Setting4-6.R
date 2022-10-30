fad79b1fdd5ba88fafb413694cfd3144d3e58519
yunxiaochen <y.chen186@lse.ac.uk>
2022-10-30 15:02:43 +0000

    Update Setting4-6.R

--- simulation/Setting4-6.R
+++ simulation/Setting4-6.R
@@ -31,8 +31,9 @@ simulation <- function(i){
   Omega <- matrix(rbinom(n*p,1,pi), n,p) 
   
   
-  M0 = temp
-  
+  M0 = data
+  M0[data <= 2] =-0.5
+  M0[data > 2] = 0.5    
   
   res1 = NBE(rho, r, data, M0, Omega, tot)
   res2 = refi.nosp(res1$M, r, data, Omega,  C2,tot)
@@ -101,8 +102,9 @@ simulation <- function(i){
   Omega <- matrix(rbinom(n*p,1,pi), n,p) 
   
   
-  M0 = temp
-  
+  M0 = data
+  M0[data <= 2] =-0.5
+  M0[data > 2] = 0.5    
   
   res1 = NBE(rho, r, data, M0, Omega, tot)
   res2 = refi.nosp(res1$M, r, data, Omega,  C2,tot)
@@ -171,8 +173,9 @@ simulation <- function(i){
   Omega <- matrix(rbinom(n*p,1,pi), n,p) 
   
   
-  M0 = temp
-  
+  M0 = data
+  M0[data <= 2] =-0.5
+  M0[data > 2] = 0.5    
   
   res1 = NBE(rho, r, data, M0, Omega, tot)
   res2 = refi.nosp(res1$M, r, data, Omega,  C2,tot)
